beec9e04b571fd8e584706365ba81a5a46c4d7bb
Kenneth Elumba <kennethelumba@gmail.com>
2022-09-02 16:33:15 +0200

    display results from artist search. create artist-search-results inside views folder

--- app.js
+++ app.js
@@ -11,10 +11,13 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-// setting the spotify-api goes here:
+// setting the spotify-api goes here: 
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+
 const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
+    clientId: clientId,
+    clientSecret: clientSecret,
     // redirectUri: 'http://www.example.com/callback'
 });
 
@@ -24,12 +27,27 @@ spotifyApi
   .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
+// simple home route that renders home page.
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/artist-search', (req, res) => {
-    
+// /artist-search route that receives the search term from the
+// query string and make a search request using a Spotify npm
+// package method.
+// artistName is the name in field input key from home page.
+app.get('/artist-search-results', (req, res, next) => {
+    const searchedArtist = req.query.artistName;
+    // console.log(searchedArtist);
+    spotifyApi
+  .searchArtists(searchedArtist)
+  .then(data => {
+    console.log('The received data from the API: ', data.body);
+    const items = data.body.artists.items;
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    res.render('artist-search-results', { allArtists: items} );
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err));
 })
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
